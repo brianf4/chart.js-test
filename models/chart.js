a1885cc243763a20a29143f33bc04f7a802d5ce1
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-09-05 14:44:23 -0700

    initial commit

---
+++ models/chart.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const moviePointsSchema = new mongoose.Schema({
+  spiderman: {
+    type: Number,
+    required: true,
+  },
+  dragonball: {
+    type: Number,
+    required: true,
+  },
+  minions: {
+    type: Number,
+    required: true,
+  },
+  jaws: {
+    type: Number,
+    required: true,
+  },
+  thor: {
+    type: Number,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('MoviePoints', moviePointsSchema, 'points')
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 4000;
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//Setting Middleware
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
