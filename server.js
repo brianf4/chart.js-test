32118404229ef53764d25bb84ba5b3d6501cbee0
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-09-12 13:52:46 -0700

    working files

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 //Setting Middleware
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
+
+
+mongoose.connect(process.env.DB_CONNECT, 
+    {userNewUrlParser: true},
+    () => {console.log('connected to db...')}
+
+)
+
+app.get('/', (req, res) => {
+    try {
+        res.render('index')
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+})
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
